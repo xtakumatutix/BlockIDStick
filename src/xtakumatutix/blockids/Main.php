149aad33c616cc57ab233a9fd855b2665a1f85d4
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2022-01-07 23:58:15 +0900

    ひとまず対応

--- src/xtakumatutix/blockids/Main.php
+++ src/xtakumatutix/blockids/Main.php
@@ -5,15 +5,18 @@ namespace xtakumatutix\blockids;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\item\Item;
+use pocketmine\item\VanillaItems;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\block\Block;
 
-Class Main extends PluginBase
+Class Main extends PluginBase implements Listener
 {
-    public function onEnable()
+    public function onEnable() :void
     {
-        $this->getLogger()->notice("読み込み完了 - ver." . $this->getDescription()->getVersion());
-        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
@@ -21,11 +24,29 @@ Class Main extends PluginBase
         if(!($sender instanceof Player)) {
             return true;
         }
-        $item = Item::get(280, 0);
+        $item = VanillaItems::STICK();
         $item->setCustomName('BlockIDStick');
         $item->setLore(['IDを調べたいブロックをこの棒でタップしてください']);
         $sender->getInventory()->addItem($item);
         $sender->sendMessage('§a >> §fBlockIDStickを追加しました');
         return true;
+    }
+
+    public function onTap(PlayerInteractEvent $event)
+    {
+        $player = $event->getPlayer();
+        $item = $event->getItem();
+        if ($item->getID() == 280 and $item->getCustomName() == 'BlockIDStick') {
+            $block = $event->getBlock();
+            $id = $block->getId();
+            if (!$id == 0){
+                $damage = $block->getMeta();
+                $blockname = $block->getName();
+                $x = $block->getPosition()->getFloorX();
+                $y = $block->getPosition()->getFloorY();
+                $z = $block->getPosition()->getFloorZ();
+                $player->sendMessage($id.":".$damage." §7(".$blockname.")§f [§c".$x." §a".$y." §b".$z. "§f]");
+            }
+        }
     }
 }
